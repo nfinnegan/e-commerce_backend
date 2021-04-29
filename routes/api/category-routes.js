2436731e9fd5132f85d48dcfb69f59ca40985b61
Natalie Finnegan <nfinneg2@gmail.com>
2021-04-29 17:19:10 -0600

    finished tag routes and tested tag & category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -63,6 +63,7 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+
 router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
   try {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -33,16 +33,50 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagDataByID = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagDataByID[0]) {
+      res.status(404).json({ message: "No tag with this id" });
+      return;
+    }
+    res.status(200).json(tagDataByID);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagDataByID = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagDataByID) {
+      res.status(404).json({ message: "No tag found with this id" });
+      return;
+    }
+    res.status(200).json(tagDataByID);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
